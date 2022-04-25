306149b2875c4bb95e6f9b242e64ec2997bc120f
edumaximo2007 <edumaximo2007@hotmail.com>
2022-04-25 10:52:08 -0300

    calculadora

---
+++ main.js
@@ -0,0 +1,41 @@
+function calcular(tipo, valor){
+    if(tipo === 'acao') {
+        //limpa o campo(idresultado)
+        if(valor === 'c'){
+            document.getElementById('resultado').value= '';
+        }
+
+        if(valor === 'ce'){
+            const dado = eval(document.getElementById('resultado').value);
+            document.getElementById('resultado').value = dado;
+            document.getElementById('resultado').value= '';
+        }
+        if(valor === 'apagar'){ 
+            document.getElementById('resultado').value= '';
+            
+        }
+        
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '%' || valor === ',') {
+            document.getElementById('resultado').value += valor;
+        }
+
+        if(valor === '**') {
+            const valor = document.getElementById('resultado').value;
+            document.getElementById('resultado').value = valor * valor;
+        }
+
+        if(valor === '²√x') {
+            const raiz = document.getElementById('resultado').value;
+            const resRaiz = Math.sqrt(raiz)
+            document.getElementById('resultado').value = resRaiz;
+        }
+
+        if(valor === '='){
+            const campo = eval(document.getElementById('resultado').value);
+            document.getElementById('resultado').value = campo;
+        }
+    } else if(tipo === 'valor') {
+        document.getElementById('resultado').value += valor;
+    }
+}
+
